b7c87bc4e11e709180a2908451c004d30ba41a19
Adrien Meyer <reyemneirda@gmail.com>
2017-12-13 14:24:23 +0200

    Created RegistrationVC class and hooked up to the StoryBoard TableView

---
+++ HotelManzana/Controllers/RegistrationTVC.swift
@@ -0,0 +1,24 @@
+//
+//  RegistrationTVC.swift
+//  HotelManzana
+//
+//  Created by Adrien Meyer on 13/12/2017.
+//  Copyright © 2017 ronny abraham. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationTVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+
+}
